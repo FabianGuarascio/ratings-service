5901bf04eaa24ef427807c736f71ae24ec69bc3c
Fabian Guarascio <guarascio.fabiand@gmail.com>
2026-06-22 17:55:28 -0300

    added the creating of a rating

--- src/ratings/ratings.module.ts
+++ src/ratings/ratings.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RatingsService } from './ratings.service';
 import { RatingsController } from './ratings.controller';
 import { PrismaService } from '../prisma.service';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_CLIENT',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'ratings-service-producer',
+            brokers: ['localhost:9092'],
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [RatingsController],
   providers: [RatingsService, PrismaService],
 })
--- src/ratings/ratings.service.ts
+++ src/ratings/ratings.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 import { PrismaService } from '../prisma.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
 
 @Injectable()
-export class RatingsService {
-  constructor(private readonly prisma: PrismaService) {}
+export class RatingsService implements OnModuleInit {
+  constructor(
+    private readonly prisma: PrismaService,
+    @Inject('KAFKA_CLIENT')
+    private readonly kafkaClient: ClientKafka,
+  ) {}
 
-  create(createRatingDto: CreateRatingDto) {
-    return this.prisma.rating.create({ data: createRatingDto });
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+  }
+
+  async create(createRatingDto: CreateRatingDto) {
+    const rating = await this.prisma.rating.create({ data: createRatingDto });
+    this.kafkaClient.emit('rating.added', {
+      movieId: rating.movieId,
+      score: rating.score,
+    });
+    return rating;
   }
 
   findAll() {
